fc1387606eefd41a2665ed3e97485e63a734b578
HIKMAL33 <himalzrzr@gmail.com>
2024-11-17 16:55:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<button id="changeColor">Ubah Warna</button>
+
+<script>
+    document.getElementById("changeColor").onclick = function() {
+        document.body.style.backgroundColor = "#ADD8E6"; // Warna biru muda
+    };
+</script>
